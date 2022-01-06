7c059b31385e87994eb948f6bf167f96c3f4852d
Ahmed M. A. Elsherbini <49863685+AhmedElsherbini@users.noreply.github.com>
2022-01-06 15:25:15 +0100

    Add files via upload

---
+++ ahmed_pipeline.sh
@@ -0,0 +1,53 @@
+
+#########################################
+#start: 12-07-2021
+#update: 16-11-2021
+echo "author:ahmed_elsherbini"
+
+#########################################
+"""
+mkdir mqc
+for f in *.fastq.gz;do fastqc -t 1 -f fastq -noextract $f;done
+mv  *html mqc/
+mv  *zip mqc/
+cd mqc/
+multiqc -z -o . .
+
+"""
+############################################################
+#if you are gonna use unicycler 
+#conda activate trim_galore
+#parallel --xapply trim_galore --illumina --paired --fastqc -o trim_galore/ ::: *_R1_001.fastq.gz ::: *_R2_002.fastq.gz
+############################################################
+#depending on which strategy you may think
+"""
+#conda activate shovill
+#conda activate unicycler 
+for file in *001.fastq.gz;
+
+do
+
+#2110-bk-d1-030_30_S30_R1_001.fastq.gz
+
+#f = what is before _R1_001.fastq.gz
+
+f=$(echo $file | sed -E "s/\_R1_001.fastq.gz*//");
+
+shovill  --trim -outdir "$f" --R1 "$f"_R1_001.fastq.gz --R2 "$f"_R2_001.fastq.gz
+
+#Unicycler has also its own advantges. right?
+#unicycler -t 12 -o "$f" --keep 2 --short1 "$f"_R1_001.fastq.gz --R2--short2 "$f"_R2_001.fastq.gz
+
+done
+"""
+
+#################################################################
+#here you will have many folders out of the assembly we will eneter loop on every folder to annotate.
+conda activate prokka
+for d in */ ; do
+    cd "$d"
+    prokka contigs.fa
+    cd ..
+done
+#################################################################
+
